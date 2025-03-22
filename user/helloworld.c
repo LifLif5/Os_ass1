68bb0d265ff69868fdc3d56577ea02d79904792d
Segevolp <olpak@post.bgu.ac.il>
2025-03-22 12:35:31 +0200

    task1

---
+++ user/helloworld.c
@@ -0,0 +1,12 @@
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    char *msg = "hello world xv6\n";
+    write(1,msg,strlen(msg));
+    exit(0);
+}
